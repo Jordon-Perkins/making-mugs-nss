9953e1432b266e8050f6e8a8d1bae0b623702a19
Jordon Perkins <jordon.rubel@gmail.com>
2023-02-09 09:58:15 -0600

    only commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const balls of clay) {
+   const mug = `${balls} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
